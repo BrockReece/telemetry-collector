d0cb63d11ae5547770245be18618b18c896d7f93
Brock <brock.reece@croud.co.uk>
2018-01-16 09:26:40 +0000

    create elastic helper module

---
+++ modules/elastic.js
@@ -0,0 +1,37 @@
+var elasticsearch = require('elasticsearch');
+
+const client = new elasticsearch.Client({
+    host: process.env.ELASTIC_HOST || '192.168.99.100:9200',
+});
+
+module.exports.buildFilters = (query) => {
+    return Object.keys(query).map((key) => {
+        const name = ['user_id'].indexOf(key) === - 1 ? `${key}.raw` : key
+        return {
+            term: { [name]: query[key], },
+        }
+    })
+}
+
+module.exports.simpleAggregation = (field, filter) => {
+    return client.search({
+        index: 'telemetry',
+        body: {
+            size: 0,
+            query: {
+                bool: {
+                    filter,
+                },
+            },
+            aggs: {
+                users: {
+                    terms: {
+                        field,
+                    },
+                }
+            }
+        },
+    })
+}
+
+module.exports.client = client
